e1c92c42473f9f0d98af1149b09639d20f197653
MRX <kurganovss666@gmail.com>
2023-07-06 20:25:40 +0300

    Считывание текста по url

--- Program.cs
+++ Program.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Net;
 
 class Program
 {
     static void Main()
     {
-        // Чтение текстового файла
-        string filePath = "C:\\Users\\kurga\\Desktop\\texrr.txt";
-        List<string> lines = File.ReadAllLines(filePath).ToList();
+        // URL для считывания текста
+        string url = "https://apps.skillfactory.ru/learning/course/course-v1:SkillFactory+CDEV+2021/block-v1:SkillFactory+CDEV+2021+type@sequential+block@df2806a9355e44c7be22a5dd8cd48818/block-v1:SkillFactory+CDEV+2021+type@vertical+block@29a2e1d179ac440a85e084c5dbf585a4";
+
+        // Считывание текста из URL
+        WebClient webClient = new WebClient();
+        string text = webClient.DownloadString(url);
+        List<string> lines = text.Split('\n').ToList();
 
         // Измерение производительности вставки в List<T>
         Stopwatch listInsertionStopwatch = Stopwatch.StartNew();
