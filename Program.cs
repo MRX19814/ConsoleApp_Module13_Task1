139e6c9a6792596ec96c9e6fdd6c59b58001705c
MRX <kurganovss666@gmail.com>
2023-07-06 16:57:59 +0300

    Текст считывается из файла

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        // Чтение текстового файла
+        string filePath = "C:\\Users\\kurga\\Desktop\\texrr.txt";
+        List<string> lines = File.ReadAllLines(filePath).ToList();
+
+        // Измерение производительности вставки в List<T>
+        Stopwatch listInsertionStopwatch = Stopwatch.StartNew();
+        List<string> list = new List<string>();
+        foreach (string line in lines)
+        {
+            list.Add(line);
+        }
+        listInsertionStopwatch.Stop();
+
+        // Измерение производительности вставки в LinkedList<T>
+        Stopwatch linkedListInsertionStopwatch = Stopwatch.StartNew();
+        LinkedList<string> linkedList = new LinkedList<string>();
+        foreach (string line in lines)
+        {
+            linkedList.AddLast(line);
+        }
+        linkedListInsertionStopwatch.Stop();
+
+        // Вывод результатов
+        Console.WriteLine("Результаты сравнения производительности вставки:");
+        Console.WriteLine($"Вставка в List<T>: {listInsertionStopwatch.Elapsed}");
+        Console.WriteLine($"Вставка в LinkedList<T>: {linkedListInsertionStopwatch.Elapsed}");
+    }
+}
